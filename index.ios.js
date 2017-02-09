c18d3b93a006af81675360f06160710d9a9cdc95
Tien Dao <daonamtien@gmail.com>
2017-02-09 22:23:55 +0700

    First commit and done add fbadk with login

---
+++ index.ios.js
@@ -0,0 +1,76 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+const FBSDK = require('react-native-fbsdk');
+const {
+  LoginButton,
+  GraphRequest,
+  GraphRequestManager,
+} = FBSDK;
+      
+var Login = React.createClass({
+  render: function() {
+    return (
+      <View>
+        <LoginButton
+          publishPermissions={["publish_actions"]}
+          onLoginFinished={
+            (error, result) => {
+              if (error) {
+                alert("Login failed with error: " + result.error);
+              } else if (result.isCancelled) {
+                alert("Login was cancelled");
+              } else {
+                alert("Login was successful with permissions: " + result.grantedPermissions)
+              }
+            }
+          }
+          onLogoutFinished={() => alert("User logged out")}/>
+        
+      </View>
+    );
+  }
+});
+export default class EventsAroundMe extends Component {
+  
+  render() {
+    return (
+      <View>
+        <Text>Welcome to the Facebook SDK for React Native!</Text>
+        <Login />
+      </View>
+
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('EventsAroundMe', () => EventsAroundMe);
