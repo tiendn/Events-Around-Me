e50109cd405e6b0fc28bdb51819631fc619e8b8b
Tien Dao <daonamtien@gmail.com>
2017-02-14 10:44:18 +0700

    Login and get Data

--- index.android.js
+++ index.android.js
@@ -11,21 +11,93 @@ import {
   Text,
   View
 } from 'react-native';
+const FBSDK = require('react-native-fbsdk');
+const {
+  LoginButton,
+  GraphRequest,
+  GraphRequestManager,
+  AccessToken
+} = FBSDK;
+var getAPI = React.createClass({
+
+})
+var Login = React.createClass({
+
+  render: function() {
+    return (
+      <View>
+        <LoginButton
+          // publishPermissions={["publish_actions,user_birthday, user_religion_politics, user_relationships, user_relationship_details, user_hometown, user_location, user_likes, user_education_history, user_work_history, user_website, user_managed_groups, user_events, user_photos, user_videos, user_friends, user_about_me, user_status, user_games_activity, user_tagged_places, user_posts, user_actions.video, user_actions.news, user_actions.books, user_actions.music, user_actions.fitness, public_profile, basic_info"]}
+          publishPermissions={["publish_actions"]}
+          onLoginFinished={
+            (error, result) => {
+              if (error) {
+                alert("Login failed with error: " + result.error);
+              } else if (result.isCancelled) {
+                alert("Login was cancelled");
+              } else {
+                console.log("Login was successful");
+                // alert("Login was successful with permissions: " + result.grantedPermissions);
+                AccessToken.getCurrentAccessToken().then(
+                  (data) => {
+                    meow_accesstoken = data.accessToken;
+                    this.testRequestGraphAPI(meow_accesstoken);
+                    console.log(meow_accesstoken.toString())
+                  }
+                )
+
+              }
+            }
+          }
+          onLogoutFinished={() => console.log("User logged out")}/>
+
+      </View>
+    );
+  },
+  _responseInfoCallback: function(error: ?Object, result: ?Object) {
+    // alert("meow response");
+    if (error) {
+      alert('Error fetching data: ' + error.toString());
+      console.log(Object.keys(error));// print all enumerable
+      console.log(error.errorMessage); // print error message
+      // error.toString() will not work correctly in this case
+      // so let use JSON.stringify()
+      meow_json = JSON.stringify(error); // error object => json
+      console.log(meow_json); // print JSON
+    } else {
+      console.log('Success fetching data: ' , result.toString());
+      console.log(Object.keys(result));
+      meow_json = JSON.stringify(result); // result => JSON
+      console.log(meow_json); // print JSON
+    }
+  },
+  testRequestGraphAPI(meow_accesstoken){
+    const infoRequest = new GraphRequest(
+      '/me',
+      {
+        parameters: {
+          fields: {
+            string: 'email,name,first_name,last_name' // what you want to get
+          },
+          access_token: {
+            string: meow_accesstoken.toString() // put your accessToken here
+          }
+        }
+      },
+      this._responseInfoCallback // make sure you define _responseInfoCallback in same class
+    );
+    new GraphRequestManager().addRequest(infoRequest).start();
+  },
+});
 
 export default class EventsAroundMe extends Component {
+ //
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+      <View>
+        <Text> Welcome to the Facebook SDK for React Native! </Text>
+        <Login />
       </View>
     );
   }
--- index.ios.js
+++ index.ios.js
@@ -14,15 +14,18 @@ import {
 const FBSDK = require('react-native-fbsdk');
 const {
   LoginButton,
+  LoginManager,
   GraphRequest,
   GraphRequestManager,
+  AccessToken
 } = FBSDK;
-      
 var Login = React.createClass({
   render: function() {
     return (
       <View>
         <LoginButton
+          // publishPermissions={["publish_actions,user_birthday, user_religion_politics, user_relationships, user_relationship_details, user_hometown, user_location, user_likes, user_education_history, user_work_history, user_website, user_managed_groups, user_events, user_photos, user_videos, user_friends, user_about_me, user_status, user_games_activity, user_tagged_places, user_posts, user_actions.video, user_actions.news, user_actions.books, user_actions.music, user_actions.fitness, public_profile, basic_info"]}
+          // publishPermissions={["publish_actions","user_events"]}
           publishPermissions={["publish_actions"]}
           onLoginFinished={
             (error, result) => {
@@ -31,25 +34,85 @@ var Login = React.createClass({
               } else if (result.isCancelled) {
                 alert("Login was cancelled");
               } else {
-                alert("Login was successful with permissions: " + result.grantedPermissions)
+                console.log("Login was successful");
+                // Attempt a login using the Facebook login dialog,
+                // asking for default permissions.
+
+                // alert("Login was successful with permissions: " + result.grantedPermissions);
+                // AccessToken.getCurrentAccessToken().then(
+                //   (data) => {
+                //     meow_accesstoken = data.accessToken;
+                //     console.log(meow_accesstoken.toString())
+                //   }
+                // )
+
               }
             }
           }
-          onLogoutFinished={() => alert("User logged out")}/>
-        
+          onLogoutFinished={() => console.log("User logged out")}/>
       </View>
     );
-  }
+  },
+
 });
+LoginManager.logInWithReadPermissions(['public_profile','user_friends','user_events']).then(
+  function(result) {
+    if (result.isCancelled) {
+      console.log('Login was cancelled');
+    } else {
+      console.log('Login was successful with permissions: '
+        + result.grantedPermissions.toString());
+
+        new GraphRequestManager().addRequest(new GraphRequest(
+          // '/search?q=Hà Nội&type=event',
+          // 'EAAaPsh5NSG4BAGUcJTWTZAoncMD8ixYEmlCjuoEAF28FRFGrrWOXlWyZAOFXOnvESQpXa97C95ifgtWaxzCbx3BYHKLEXjZCxjXguVByY8zqVmWY977ZC63lzYlIQKZCbG6ilTLkX2Ss5Hcwra1X6Ynm9AaLI5ZATKeujCMDnmvAbY4pakvZCySZCjafT8iYZBj8ZD',
+          // '/me?metadata=1',
+          // '/me/events',
+          '/search',
+          // 'GET',
+          {"q":"Hà Nội","type":"event"},
+          // null,
+          function(error: ?Object, result: ?Object) {
+            if (error) {
+              console.log('Error fetching data: ' , error);
+            } else {
+              console.log('Success fetching ddata: ' , result);
+            }
+          },
+        )).start()
+    }
+  },
+  function(error) {
+    console.log('Login failed with error: ' + error);
+  }
+);
+// testRequestGraphAPI(meow_accesstoken){
+// const infoRequest = new GraphRequest(
+//   // '/search',
+//   // 'EAAaPsh5NSG4BAGUcJTWTZAoncMD8ixYEmlCjuoEAF28FRFGrrWOXlWyZAOFXOnvESQpXa97C95ifgtWaxzCbx3BYHKLEXjZCxjXguVByY8zqVmWY977ZC63lzYlIQKZCbG6ilTLkX2Ss5Hcwra1X6Ynm9AaLI5ZATKeujCMDnmvAbY4pakvZCySZCjafT8iYZBj8ZD',
+//   // '/me?metadata=1',
+//   '/me/friends',
+//   null,
+//   function(error: ?Object, result: ?Object) {
+//     if (error) {
+//       console.log('Error fetching data: ' , error);
+//     } else {
+//       console.log('Success fetching data: ' , result);
+//     }
+//   },
+// );
+// new GraphRequestManager().addRequest(infoRequest).start();
+
+// }
 export default class EventsAroundMe extends Component {
-  
+ //
+
   render() {
     return (
       <View>
-        <Text>Welcome to the Facebook SDK for React Native!</Text>
+        <Text> Welcome to the Facebook SDK for React Native! </Text>
         <Login />
       </View>
-
     );
   }
 }
