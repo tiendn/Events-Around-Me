0966d23755b0c091e1382a205ff851f2dda15c40
Tien Dao <daonamtien1@gmail.com>
2017-04-04 23:42:07 +0700

    Version 1.6 - Update React-native 0.43

--- android/app/src/main/java/com/eventsaroundme/MainApplication.java
+++ android/app/src/main/java/com/eventsaroundme/MainApplication.java
@@ -1,11 +1,13 @@
 package com.eventsaroundme;
 
 import android.app.Application;
-import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+<<<<<<< ours
 import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.facebook.react.ReactInstanceManager;
+=======
+>>>>>>> theirs
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
